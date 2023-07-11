8308e433a482d13417d78832fcaf60a8bd341a92
Mingjie Li <mingjli@ethz.ch>
2023-07-11 21:01:20 +0200

    plotting data

---
+++ assets/plotting_utils/plotting_wave_utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+# change d to get desire plotting data
+d = 1
+res = 64
+data = np.load(f"./wave_plotting_data_d{d}.npy")
+print(data[0])
+input_x = data[:, :2]
+mu = data[0, 2:2+d*d]
+u_0 = data[:, 2+d*d]
+u_T = data[:, 3+d*d]
+
+print("Inputs:", input_x.shape, "\n mu:", mu.shape, "\n u_0:", u_0.shape, "\n u_T:", u_T.shape)
+
+fig, axs = plt.subplots(1, 2, figsize=(16, 6), dpi=150)
+im1 = axs[0].contourf(input_x[:, 0].reshape(res,res), input_x[:, 1].reshape(res,res), u_0.reshape(res,res), levels = 64, cmap="jet")
+axs[0].set_xlabel("x1")
+axs[0].set_ylabel("x2")
+axs[0].set_title("u_0")
+plt.colorbar(im1, ax=axs[0])
+axs[0].grid(True, which="both", ls=":")
+im2 = axs[1].contourf(input_x[:, 0].reshape(res,res), input_x[:, 1].reshape(res,res), u_T.reshape(res,res), levels = 64, cmap="jet")
+axs[1].set_xlabel("x1")
+axs[1].set_ylabel("x2")
+axs[1].set_title("u_T")
+plt.colorbar(im2, ax=axs[1])
+axs[1].grid(True, which="both", ls=":")
+plt.show()
